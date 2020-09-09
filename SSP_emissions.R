278b31ecdb711a1c21d3607c8849f6c62f176492
Hartin <Hartin.Corinne@epa.gov>
2020-09-09 10:30:39 -0400

    renamed emissions and converted units

--- SSP_emissions.R
+++ SSP_emissions.R
@@ -23,19 +23,20 @@ ssps <- read.csv("C:/Users/chartin/Documents/GitHub/SSP_emissions/rcmip-emission
   filter(Region == "World" & Mip_Era == "CMIP6") %>% 
   select(-Region, -Mip_Era) %>% 
   filter(Scenario %in% c( "ssp119", "ssp370", "ssp126", "ssp245","ssp434", "ssp460", "ssp534-over", "ssp580")) %>% 
-  filter(Variable %in% c("Emissions|CO2","Emissions|CO2|MAGICC AFOLU", "Emissions|CH4",  "Emissions|N2O", "Emissions|Sulfur", "Emissions|CO",
-                         "Emissions|VOC", "Emissions|NOx", "Emissions|BC", "Emissions|OC", "Emissions|F-Gases|PFC|CF4", "Emissions|F-Gases|PFC|C2F6", 
-                         "Emissions|F-Gases|HFC|HFC23",
-                         "Emissions|F-Gases|HFC|HFC32", "Emissions|F-Gases|HFC|HFC32", "Emissions|F-Gases|HFC|HFC4310mee", "Emissions|F-Gases|HFC|HFC125",
-                         "Emissions|F-Gases|HFC|HFC134a", 
-                         "Emissions|F-Gases|HFC|HFC143a", "Emissions|F-Gases|HFC|HFC227ea",  "Emissions|F-Gases|HFC|HFC245fa", "Emissions|F-Gases|SF6", 
+  filter(Variable %in% c("Emissions|CO2","Emissions|CO2|MAGICC AFOLU", "Emissions|CH4",  "Emissions|N2O", 
+                         "Emissions|Sulfur", "Emissions|CO", "Emissions|VOC", "Emissions|NOx", 
+                         "Emissions|BC", "Emissions|OC", "Emissions|F-Gases|PFC|CF4", "Emissions|F-Gases|PFC|C2F6", 
+                         "Emissions|F-Gases|HFC|HFC23", "Emissions|F-Gases|HFC|HFC32", "Emissions|F-Gases|HFC|HFC32", 
+                         "Emissions|F-Gases|HFC|HFC4310mee", "Emissions|F-Gases|HFC|HFC125",
+                         "Emissions|F-Gases|HFC|HFC134a", "Emissions|F-Gases|HFC|HFC143a", "Emissions|F-Gases|HFC|HFC227ea",  
+                         "Emissions|F-Gases|HFC|HFC245fa", "Emissions|F-Gases|SF6", 
                          "Emissions|Montreal Gases|CFC|CFC11", "Emissions|Montreal Gases|CFC|CFC12", "Emissions|Montreal Gases|CFC|CFC113", 
-                         "Emissions|Montreal Gases|CFC|CFC114",
-                         "Emissions|Montreal Gases|CFC|CFC115",  "Emissions|Montreal Gases|CCl4", "Emissions|Montreal Gases|CCl4", 
+                         "Emissions|Montreal Gases|CFC|CFC114","Emissions|Montreal Gases|CFC|CFC115",  
+                         "Emissions|Montreal Gases|CCl4", "Emissions|Montreal Gases|CCl4", 
                          "Emissions|Montreal Gases|CH3CCl3", "Emissions|Montreal Gases|HCFC22",
                          "Emissions|Montreal Gases|HCFC141b", "Emissions|Montreal Gases|HCFC142b", "Emissions|Montreal Gases|Halon1211",  
-                         "Emissions|Montreal Gases|Halon1301",
-                         "Emissions|Montreal Gases|Halon2402", "Emissions|Montreal Gases|CH3Br",  "Emissions|Montreal Gases|CH3Cl")) %>% 
+                         "Emissions|Montreal Gases|Halon1301", "Emissions|Montreal Gases|Halon2402", 
+                         "Emissions|Montreal Gases|CH3Br",  "Emissions|Montreal Gases|CH3Cl")) %>% 
   select(Scenario, Variable, Unit, X2015:X2300) 
 
 historical <- read.csv("C:/Users/chartin/Documents/GitHub/SSP_emissions/rcmip-emissions-annual-means-v4-0-0.csv") %>% 
@@ -43,97 +44,77 @@ historical <- read.csv("C:/Users/chartin/Documents/GitHub/SSP_emissions/rcmip-em
   filter(Region == "World" & Mip_Era == "CMIP6") %>% 
   select(-Region, -Mip_Era) %>% 
   filter(Scenario %in% c( "historical")) %>% 
-  filter(Variable %in% c("Emissions|CO2","Emissions|CO2|MAGICC AFOLU", "Emissions|CH4",  "Emissions|N2O", "Emissions|Sulfur", "Emissions|CO",
-                         "Emissions|VOC", "Emissions|NOx", "Emissions|BC", "Emissions|OC", "Emissions|F-Gases|PFC|CF4", "Emissions|F-Gases|PFC|C2F6", 
-                         "Emissions|F-Gases|HFC|HFC23",
-                         "Emissions|F-Gases|HFC|HFC32", "Emissions|F-Gases|HFC|HFC32", "Emissions|F-Gases|HFC|HFC4310mee", "Emissions|F-Gases|HFC|HFC125",
-                         "Emissions|F-Gases|HFC|HFC134a", 
-                         "Emissions|F-Gases|HFC|HFC143a", "Emissions|F-Gases|HFC|HFC227ea",  "Emissions|F-Gases|HFC|HFC245fa", "Emissions|F-Gases|SF6", 
+  filter(Variable %in% c("Emissions|CO2","Emissions|CO2|MAGICC AFOLU", "Emissions|CH4",  "Emissions|N2O", 
+                         "Emissions|Sulfur", "Emissions|CO", "Emissions|VOC", "Emissions|NOx", 
+                         "Emissions|BC", "Emissions|OC", "Emissions|F-Gases|PFC|CF4", "Emissions|F-Gases|PFC|C2F6", 
+                         "Emissions|F-Gases|HFC|HFC23", "Emissions|F-Gases|HFC|HFC32", "Emissions|F-Gases|HFC|HFC32", 
+                         "Emissions|F-Gases|HFC|HFC4310mee", "Emissions|F-Gases|HFC|HFC125",
+                         "Emissions|F-Gases|HFC|HFC134a", "Emissions|F-Gases|HFC|HFC143a", "Emissions|F-Gases|HFC|HFC227ea",  
+                         "Emissions|F-Gases|HFC|HFC245fa", "Emissions|F-Gases|SF6", 
                          "Emissions|Montreal Gases|CFC|CFC11", "Emissions|Montreal Gases|CFC|CFC12", "Emissions|Montreal Gases|CFC|CFC113", 
-                         "Emissions|Montreal Gases|CFC|CFC114",
-                         "Emissions|Montreal Gases|CFC|CFC115",  "Emissions|Montreal Gases|CCl4", "Emissions|Montreal Gases|CCl4", 
+                         "Emissions|Montreal Gases|CFC|CFC114","Emissions|Montreal Gases|CFC|CFC115",  
+                         "Emissions|Montreal Gases|CCl4", "Emissions|Montreal Gases|CCl4", 
                          "Emissions|Montreal Gases|CH3CCl3", "Emissions|Montreal Gases|HCFC22",
                          "Emissions|Montreal Gases|HCFC141b", "Emissions|Montreal Gases|HCFC142b", "Emissions|Montreal Gases|Halon1211",  
-                         "Emissions|Montreal Gases|Halon1301",
-                         "Emissions|Montreal Gases|Halon2402", "Emissions|Montreal Gases|CH3Br",  "Emissions|Montreal Gases|CH3Cl", 
-                         "Emissions|F-Gases|HFC|HFC236fa")) %>% 
+                         "Emissions|Montreal Gases|Halon1301", "Emissions|Montreal Gases|Halon2402", 
+                         "Emissions|Montreal Gases|CH3Br",  "Emissions|Montreal Gases|CH3Cl")) %>%
   select(Scenario, Variable, Unit, X1750:X2014)
 
 
-###################################
-# next up
-# 1. what variables are in hector
-# 2. pull out each scenario
-# 3. need to check and update units
-#     a. CO2 from MtCO2 to GtC
-#     b. N2O from ktN2O to MtN2O
-#     c. SO2 from Mt to Gg (is it S or SO2?)
-# 4. flip to long/wide to match Hector
-
 # Gases included in RCMIP but not included in Hector
-"Emissions|NH3"
-"Emissions|F-Gases|HFC|HFC152a"
-"Emissions|F-Gases|HFC|HFC365mfc"
-"Emissions|F-Gases|NF3"
-"Emissions|F-Gases|PFC|C3F8"
-"Emissions|F-Gases|PFC|C4F10"
-"Emissions|F-Gases|PFC|C5F12"
-"Emissions|F-Gases|PFC|C6F14"
-"Emissions|F-Gases|PFC|C7F16"
-"Emissions|F-Gases|PFC|C8F18"
-"Emissions|F-Gases|PFC|cC4F8" 
-"Emissions|F-Gases|SO2F2"
-"Emissions|Montreal Gases|CH2Cl2"
-"Emissions|Montreal Gases|CHCl3"
-"Emissions|Montreal Gases|Halon1202"
-"Emissions|F-Gases|HFC|HFC236fa"
+# "Emissions|NH3"
+# "Emissions|F-Gases|HFC|HFC152a"
+# "Emissions|F-Gases|HFC|HFC365mfc"
+# "Emissions|F-Gases|NF3"
+# "Emissions|F-Gases|PFC|C3F8"
+# "Emissions|F-Gases|PFC|C4F10"
+# "Emissions|F-Gases|PFC|C5F12"
+# "Emissions|F-Gases|PFC|C6F14"
+# "Emissions|F-Gases|PFC|C7F16"
+# "Emissions|F-Gases|PFC|C8F18"
+# "Emissions|F-Gases|PFC|cC4F8" 
+# "Emissions|F-Gases|SO2F2"
+# "Emissions|Montreal Gases|CH2Cl2"
+# "Emissions|Montreal Gases|CHCl3"
+# "Emissions|Montreal Gases|Halon1202"
+# "Emissions|F-Gases|HFC|HFC236fa"
 
-# Gases included in Hector but not RCMIP
-# C2F14
-# C2F6
 
 #################################################
-#transpose dataframe so the years on the rows and the variables are columns
+# pull out 1 scenario at a time
 ssp119 <- filter(ssps, Scenario == "ssp119") %>% 
-  select(-Scenario)
+  select(-Scenario, -Unit) %>% 
+  
+  ## get rid of strings before gas name
+  mutate(Variable=as.character(Variable)) %>%
+  mutate(Variable=(str_replace(Variable, "^.*\\|", ""))) %>% 
+  
+  # add "_emissions" to gas names
+  mutate(Variable=paste0(Variable, "_emissions")) %>% 
+  gather(Date, value, X2015:X2300) %>% 
+  spread(Variable, value) %>% 
+  mutate(Date=(str_replace(Date, "X", ""))) %>% # get rid of X in front of date
+  
+  ## Rename a few columns to match Hector
+  
+  rename("ffi_emissions" = "CO2_emissions") %>% 
+  rename("luc_emissions" = "MAGICC AFOLU_emissions") %>%
+  rename("NMVOC_emissions" = "VOC_emissions") %>% 
+  rename("HFC4310_emissions"="HFC4310mee_emissions") %>% 
+  rename( "SO2_emissions" ="Sulfur_emissions") %>% 
+  
+  ## Unit changes
+  mutate(ffi_emissions = ((ffi_emissions/1000) *(12/44))) %>%  # Convert from MtCO2 to GtC/year
+  mutate(N2O_emissions = ((N2O_emissions * 0.001) *(14.0067/44.0128))) %>%  # convert from ktN2O to GtN
+  mutate(SO2_emissions = ((SO2_emissions * 1000) * (32.01/64.07))) %>%  # convert from MtSO2 to GgS
+  
+  ## save as a csv file
+  write.csv("Github/SSP_emissions/ssp119.csv", row.names = FALSE)
+  
 
-ssp119 <- as.data.frame(t(as.matrix(ssp119))) %>% 
-  rename("BC_emissions" = "V1") %>% 
-  rename("CH4_emissions" = "V2") %>% 
-  rename("CO_emissions" = "V3") %>% 
-  rename("ffi_emissions" = "V4") %>% 
-  rename("luc_emissions" = "V5") %>% 
-  rename("HFC125_emissions" = "V6") %>% 
-  rename("HFC134a_emissions" = "V7") %>% 
-  rename("HFC143a_emissions" = "V8") %>% 
-  rename("HFC227ea_emissions" = "V9") %>% 
-  rename("HFC23_emissions" = "V10") %>% 
-  rename("HFC245fa_emissions" = "V11") %>% 
-  rename("HFC32_emissions" = "V12") %>% 
-  rename("HFC4310_emissions" = "V13") %>% 
-  rename("C2F6_emissions" = "V14") %>% 
-  rename("CF4_emissions" = "V15") %>% 
-  rename("SF6_emissions" = "V16") %>% 
-  rename("CCl4_emissions" = "V17") %>% 
-  rename("CFC11_emissions" = "V18") %>% 
-  rename("CFC113_emissions" = "V19") %>% 
-  rename("CFC114_emissions" = "V20") %>% 
-  rename("CFC115_emissions" = "V21") %>% 
-  rename("CFC12_emissions" = "V22") %>% 
-  rename("CH3Br_emissions" = "V23") %>% 
-  rename("CH3CCl3_emissions" = "V24") %>% 
-  rename("CH3Cl_emissions" = "V25") %>% 
-  rename("HCFC141b_emissions" = "V26") %>% 
-  rename("HCFC142b_emissions" = "V27") %>% 
-  rename("HCFC22_emissions" = "V28") %>% 
-  rename("halon1211_emissions" = "V29") %>% 
-  rename("halon1301_emissions" = "V30") %>% 
-  rename("halon2402_emissions" = "V31") %>% 
-  rename("N2O_emissions" = "V32") %>% 
-  rename("NOX_emissions" = "V33") %>% 
-  rename("OC_emissions" = "V34") %>% 
-  rename("SO2_emissions" = "V35") %>% 
-  rename("NMVOC_emissions" = "V36")
+
+
+
 
 ssp370 <- filter(ssps, Scenario == "ssp370") %>% 
   select(-Scenario)
