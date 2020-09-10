6de8afced0e22e25754e4db2a4622008b18f44ae
Hartin <Hartin.Corinne@epa.gov>
2020-09-10 10:52:00 -0400

    ssp119 ini file

--- SSP_emissions.R
+++ SSP_emissions.R
@@ -5,23 +5,15 @@
 library(tidyr)
 library(dplyr)
 library(stringr)
-#path = "C:/Users/chartin/Documents/GitHub/SSP_emissions/"
 
-Hector_emis <- c("BC_emissions","CH4_emissions", "CO_emissions", "ffi_emissions","luc_emissions",
-                 "HFC125_emissions", "HFC134a_emissions","HFC143a_emissions",
-                 "HFC227ea_emissions",  "HFC23_emissions", "HFC245fa_emissions", "HFC32_emissions",
-                 "HFC4310_emissions",  "C2F6_emissions", "CF4_emissions","SF6_emissions",
-                 "CCl4_emissions", "CFC11_emissions",	"CFC113_emissions", "CFC114_emissions",	"CFC115_emissions",	"CFC12_emissions",
-                 "CH3Br_emissions","CH3CCl3_emissions", "CH3Cl_emissions", "HCFC141b_emissions",	"HCFC142b_emissions",
-                 "HCFC22_emissions",	"halon1211_emissions", "halon1301_emissions", "halon2402_emissions",
-                 "N2O_emissions", "NOX_emissions", "OC_emissions", "SO2_emissions","NMVOC_emissions")
+setwd("C:/Users/chartin/Documents/GitHub/SSP_emissions/")
 
 ###########################
 scenarios <- c("ssp119", "ssp370", "ssp126", "ssp245","ssp434", "ssp460", "ssp534-over", "ssp585")
 
 ###############################
 
-ssps <- read.csv("C:/Users/chartin/Documents/GitHub/SSP_emissions/rcmip-emissions-annual-means-v4-0-0.csv") %>% 
+ssps <- read.csv("rcmip-emissions-annual-means-v4-0-0.csv") %>% 
   select(-Model, -Activity_Id) %>% 
   filter(Region == "World" & Mip_Era == "CMIP6") %>% 
   select(-Region, -Mip_Era) %>% 
@@ -66,36 +58,6 @@ ssps <- read.csv("C:/Users/chartin/Documents/GitHub/SSP_emissions/rcmip-emission
 #################################################
 
 # pull out 1 scenario at a time - should be able to loop. but it's not working at the moment
-hist <- filter(ssps, Scenario == "historical") %>% 
-  select(-Scenario, -Unit) %>% 
-  
-  ## get rid of strings before gas name
-  mutate(Variable=as.character(Variable)) %>%
-  mutate(Variable=(str_replace(Variable, "^.*\\|", ""))) %>% 
-  
-  # add "_emissions" to gas names
-  mutate(Variable=paste0(Variable, "_emissions")) %>% 
-  gather(Date, value, X1750:X2014) %>% 
-  spread(Variable, value) %>% 
-  mutate(Date=(str_replace(Date, "X", ""))) %>% # get rid of X in front of date
-  
-  ## Rename a few columns to match Hector
-  
-  rename("ffi_emissions" = "CO2_emissions") %>% 
-  rename("luc_emissions" = "MAGICC AFOLU_emissions") %>%
-  rename("NMVOC_emissions" = "VOC_emissions") %>% 
-  rename("HFC4310_emissions"="HFC4310mee_emissions") %>% 
-  rename( "SO2_emissions" ="Sulfur_emissions") %>% 
-  
-  ## Unit changes
-  mutate(ffi_emissions = ((ffi_emissions/1000) *(12/44))) %>%  # Convert from MtCO2 to GtC/year
-  mutate(N2O_emissions = ((N2O_emissions * 0.001) *(14.0067/44.0128))) %>%  # convert from ktN2O to GtN
-  mutate(SO2_emissions = ((SO2_emissions * 1000) * (32.01/64.07)))  # convert from MtSO2 to GgS
-
-cat("; historical emissions \n ; https://www.rcmip.org/ \n ",file="hist_emissions.csv")
-write.table(hist, 'hist_emissions.csv',sep=",",append=TRUE, row.names=FALSE)
-
-#########################
 ssp119 <- filter(ssps, Scenario == "ssp119") %>% 
   select(-Scenario, -Unit) %>% 
   
@@ -171,7 +133,7 @@ ssp119 <- filter(ssps, Scenario == "ssp119") %>%
     
     # add "_emissions" to gas names
     mutate(Variable=paste0(Variable, "_emissions")) %>% 
-    gather(Date, value, X2015:X2300) %>% 
+    gather(Date, value, X1750:X2300) %>% 
     spread(Variable, value) %>% 
     mutate(Date=(str_replace(Date, "X", ""))) %>% # get rid of X in front of date
     
